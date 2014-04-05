dd4f3926278ac51fa0715afdfb7eb8b9eb445c2e
Dario Vidal <vidaldm@gmail.com>
2014-04-05 02:00:19 -0700

    Added results

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -11,19 +11,32 @@ namespace MyFirstProgram
         static void Main(string[] args)
         {
             Console.WriteLine("HIKyle");
+            
             Console.WriteLine("what is your name?");
             string yourName;
             yourName = Console.ReadLine();
             Console.WriteLine("OK I will call you {0}", yourName);
+            
             Console.WriteLine("What Is your Favorite Number?");
             int favoriteNumber;
             favoriteNumber = int.Parse(Console.ReadLine());
             Console.WriteLine("{0} is a cool number", favoriteNumber);
+            
             Random randomNumberGenerator = new Random();
             Console.WriteLine("My Favorite Number Is {0}. it's random, I know", randomNumberGenerator.Next());
+            
             Console.WriteLine("What is your favorite Car?");
             string yourFavoriteCar;
             yourFavoriteCar = Console.ReadLine();
+            
+            Console.WriteLine("Give me a Number");
+            int number1;
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give me another number");
+            int number2;
+            number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}", number1, number2, number1 + number2);
+
             Console.ReadLine();
         }
     }
